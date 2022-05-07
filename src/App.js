2de1fe47222ed66691e9bb25500a5b300c694b02
Omar Elbaga <hello@uiworkspace.com>
2022-05-07 01:52:16 -0400

    refine first heroplain component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Heroplain from "./heros/src/components/Heroplain/Heroplain.js";
+import "./App.scss";
 // import { Heroplain } from "@uiworkspace/heros";
 
 function App() {
+  const heroPlainContent = {
+    bgcolor: "white",
+    bgimage: "/people.jpg",
+    bgimageOverlay: 0.3,
+  };
   return (
     <div className="App">
-      <Heroplain bgcolor="rgb(234, 114, 88)">Hello World</Heroplain>
+      <Heroplain styles={heroPlainContent}>
+        <div className="headline">Explore Your World</div>
+      </Heroplain>
     </div>
   );
 }
--- src/heros/src/components/Heroplain/Heroplain.js
+++ src/heros/src/components/Heroplain/Heroplain.js
@@ -2,12 +2,43 @@ import React from "react";
 import styles from "./Heroplain.module.scss";
 
 function Heroplain(props) {
+  let bgcolor;
+  let bgimage;
+  let bgimageOverlay;
+  let bgcolorDefaultValue = "white";
+  let bgimageDefaultValue =
+    "https://uiworkspace.com/images/uiw-placeholder1.jpg";
+  let bgimageOverlayDefaultValue = 0.3;
+
+  if (props.styles) {
+    if (props.styles.bgcolor) {
+      bgcolor = props.styles.bgcolor;
+    } else {
+      bgcolor = bgcolorDefaultValue;
+    }
+    if (props.styles.bgimage) {
+      bgimage = props.styles.bgimage;
+    } else {
+      bgimage = bgimageDefaultValue;
+    }
+    if (props.styles.bgimageOverlay) {
+      bgimageOverlay = props.styles.bgimageOverlay;
+    } else {
+      bgimageOverlay = bgimageOverlayDefaultValue;
+    }
+  } else if (props.styles === undefined) {
+    bgimage = bgimageDefaultValue;
+    bgcolor = bgcolorDefaultValue;
+    bgimageOverlay = bgimageOverlayDefaultValue;
+  }
+
+  const stylesProps = {
+    backgroundColor: bgcolor,
+    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, ${bgimageOverlay}), rgba(0, 0, 0, ${bgimageOverlay})), url(${bgimage})`,
+  };
   return (
     <>
-      <section
-        className={`${styles.section}`}
-        style={{ backgroundColor: props.bgcolor }}
-      >
+      <section className={`${styles.section}`} style={stylesProps}>
         <div className={styles.holdcontent}>{props.children}</div>
       </section>
     </>
